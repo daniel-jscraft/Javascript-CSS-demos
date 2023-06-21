2e6fbf9c1f844a93d6c8c993c5a938f700015753
Daniel <daniel.nastase@gmail.com>
2023-06-21 11:48:46 +0300

    example ready

--- demos/📓_nextjs/02-server-actions-fetch/src/app/page.js
+++ demos/📓_nextjs/02-server-actions-fetch/src/app/page.js
@@ -1,9 +1,25 @@
-"use client"
+const api = 'https://jsonplaceholder.typicode.com/'
 
 export default function Home() {
+
+  const fetchData = async (formData)=> {
+    'use server'
+    const itemsType = formData.get('items-type')
+    const response = await fetch(api + itemsType)
+    const data = await response.json()
+    console.log(data)
+  }
+
   return (
-    <button onClick={() => alert('hi')}>
-      Say hi 👋!!!
-    </button>
+    <form action={fetchData}>
+      <select name="items-type">
+        <option value="posts">📰 Posts</option>
+        <option value="comments">💬 Comments</option>
+        <option value="albums">📚 Albums</option>
+        <option value="photos">📸 Photos</option>
+        <option value="users">👨 Users</option>
+      </select>
+      <button>Go get the data</button>
+    </form>
   )
 }
