6383a0b08edd3eec23962666e93b61993746678c
Daniel <daniel.nastase@gmail.com>
2023-06-23 10:49:05 +0300

    use state not needed

--- demos/📓_nextjs/03-infinite-scroll/src/app/page.js
+++ demos/📓_nextjs/03-infinite-scroll/src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useInfiniteQuery, QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { useState, useRef, useEffect } from "react"
+import { useRef, useEffect } from "react"
 
 const fetchData = async (page) => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5&_page=' + page)
@@ -55,7 +55,6 @@ const MyComponent = ()=> {
         <span ref={myRef}></span>
       </ol>
     </div>
-    
   </>
 }
 
