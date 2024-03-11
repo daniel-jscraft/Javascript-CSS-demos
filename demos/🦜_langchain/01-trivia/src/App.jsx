87a4d9a8f2bf95d6b9d25bb761df5fd0270e936c
Daniel <daniel.nastase@gmail.com>
2024-03-11 16:30:06 +0200

    lagchain conversation

--- demos/🦜_langchain/01-trivia/src/App.jsx
+++ demos/🦜_langchain/01-trivia/src/App.jsx
@@ -2,50 +2,59 @@ import { useEffect, useRef, useState } from 'react'
 import { ChatOpenAI } from "@langchain/openai"
 import { ChatPromptTemplate } from "@langchain/core/prompts"
 import { CommaSeparatedListOutputParser } from "@langchain/core/output_parsers"
+import { ConversationChain } from "langchain/chains";
+import { BufferMemory } from "langchain/memory";
+import { BaseMessage } from "@langchain/core/messages"
+
+const memory = new BufferMemory();
 
 const model = new ChatOpenAI({
   openAIApiKey: import.meta.env.VITE_OPENAI_KEY, 
 })
 
+const chain = new ConversationChain({ llm: model, memory })
+
 function App() {
   const [domain, setDomain ] = useState('history')
   const [question, setQuestion] = useState()
+  const [hint, setHint] = useState(null)
+  const [result, setResult] = useState(null)
 
   const gptAskQuestion = async () => {
-    const prompt = ChatPromptTemplate.fromMessages([
-        [
-            "system",
-            "Ask an easy trivia question from {domain}. "
-        ],
-        ["human", "{domain}"],
-    ])
-    const chain = prompt.pipe(model)
-    const response = await chain.invoke({ domain: domain})
-    setQuestion(response.content)
-  }
-
-  const gptVerifyAswer = async (answer) => {
-    const prompt = ChatPromptTemplate.fromMessages([
-      [
-          "system",
-          "Evaluate the answer: {answer} for the question: {question}. Respond just with True or False."
-      ],
-      ["human", "{answer}", "{question}"]
-    ])
-    const chain = prompt.pipe(model)
-    const response = await chain.invoke({ 
-      question, 
-      answer
+    const response = await chain.call({
+      input: `Give me an easy trivia question from ${domain}.` 
     })
     console.log(response)
+    setQuestion(response.response)
+    setResult(null)
+    setHint(null)
+  }
+
+
+  /*
+  Give me a just a hint for {question}, but not the answer
+  "Give me a another, different from the last one, for {question}, but not the answer"
+  */
+
+  const gptHint = async () => {
+    const response = await chain.call({
+      input: `Give me a just a hint for the last question, but not the answer.` 
+    })
+    console.log(response)
+    setHint(response.response)
   }
 
   const onDomainChangeHandler = (evt) => setDomain(evt.target.value)
 
-  const onSubmitHandler = (event)=> {
+  const onSubmitHandler = async (event)=> {
     event.preventDefault()
     const answer = event.target.answer.value
-    gptVerifyAswer(answer)
+
+    const response = await chain.call({
+      input: `Is ${answer} the correct answer for the question? Just reply with correct or  incorrect.` 
+    })
+    console.log(response)
+    setResult(response.response)
   }
 
   return (
@@ -71,6 +80,10 @@ function App() {
             placeholder='answer ...'  />
           <button>Check answer</button>
         </form>
+        <button onClick={gptHint}>🤨 Give me a Hint please!</button>
+
+        <p>{hint}</p>
+        <p>{result && (result.toLowerCase() === 'correct' ? '✅ That is Correct!' : '❌ Nope, this is false!')}</p>
       </div>}
     </>
   )
