a4201c8c380ce082cbe318dcf8d22418fa2de735
Daniel <daniel.nastase@gmail.com>
2024-06-17 11:55:20 +0300

    04-nested-chains poc

--- demos/🦜_langchain/04-nested-chains/index.js
+++ demos/🦜_langchain/04-nested-chains/index.js
@@ -1,20 +1,33 @@
 import { ChatOpenAI } from "@langchain/openai"
 import { StringOutputParser } from "@langchain/core/output_parsers"
 import { PromptTemplate } from "@langchain/core/prompts"
+import { RunnableSequence } from "@langchain/core/runnables"
 import * as dotenv from "dotenv"
 
 dotenv.config()
 
 const model = new ChatOpenAI()
 
-const stringOutputParser = new StringOutputParser()
-
-const prompt = PromptTemplate.fromTemplate(
-    `What is the best gangster movie?`
+const prompt1 = PromptTemplate.fromTemplate(
+    `Which country has the highest population in Latin America? Respond only with the name of the country.`
 )
 
-const chain = prompt.pipe(model).pipe(stringOutputParser)
+console.log(prompt1)
+const chain1 = prompt1
+                .pipe(model)
+                .pipe(new StringOutputParser())
 
-const response = await chain.invoke()
+const prompt2 = PromptTemplate.fromTemplate(
+    `Tell me the currency of {country}?`
+)
 
+const completeChain = RunnableSequence.from([
+    { country: chain1 },
+    prompt2,
+    model,
+    new StringOutputParser(),
+])
+
+const response = await completeChain.invoke()
 console.log(response)
+
