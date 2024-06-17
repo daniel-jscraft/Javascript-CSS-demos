0ecbb4ad488c429381224e5095fdce5c60b87c8f
Daniel <daniel.nastase@gmail.com>
2024-06-17 10:27:05 +0300

    04-nested-chains build chain with pipe

--- demos/🦜_langchain/04-nested-chains/index.js
+++ demos/🦜_langchain/04-nested-chains/index.js
@@ -1,10 +1,20 @@
-import { ChatOpenAI } from "@langchain/openai";
+import { ChatOpenAI } from "@langchain/openai"
+import { StringOutputParser } from "@langchain/core/output_parsers"
+import { PromptTemplate } from "@langchain/core/prompts"
 import * as dotenv from "dotenv"
 
 dotenv.config()
 
 const model = new ChatOpenAI()
 
-const response = await model.invoke('What is the best gangster movie?')
+const stringOutputParser = new StringOutputParser()
+
+const prompt = PromptTemplate.fromTemplate(
+    `What is the best gangster movie?`
+)
+
+const chain = prompt.pipe(model).pipe(stringOutputParser)
+
+const response = await chain.invoke()
 
 console.log(response)
