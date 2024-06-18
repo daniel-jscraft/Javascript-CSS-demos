ef1528913ac03e47ffa75c61d1b0385c67a654f8
Daniel <daniel.nastase@gmail.com>
2024-06-18 13:37:20 +0300

    remove comments

--- demos/🦜_langchain/04-nested-chains/index.js
+++ demos/🦜_langchain/04-nested-chains/index.js
@@ -1,73 +1,3 @@
-// import { ChatOpenAI } from "@langchain/openai"
-// import { StringOutputParser } from "@langchain/core/output_parsers"
-// import { PromptTemplate } from "@langchain/core/prompts"
-// import { RunnableSequence } from "@langchain/core/runnables"
-// import * as dotenv from "dotenv"
-
-// dotenv.config()
-
-// const model = new ChatOpenAI()
-
-// const prompt1 = PromptTemplate.fromTemplate(
-//     `Which country has the highest population in Latin America? Respond only with the name of the country.`
-// )
-
-// const chain1 = prompt1
-//                 .pipe(model)
-//                 .pipe(new StringOutputParser())
-
-// const prompt2 = PromptTemplate.fromTemplate(
-//     `Tell me the currency of {country}?`
-// )
-
-// const completeChain = RunnableSequence.from([
-//     { country: chain1 },
-//     prompt2,
-//     model,
-//     new StringOutputParser(),
-// ])
-
-// const response = await completeChain.invoke()
-// console.log(response)
-
-
-// or we can extract / merge content from the RunnableSequence into a separte chain. The second chain will use the input from the first chain. 
-// import { ChatOpenAI } from "@langchain/openai"
-// import { StringOutputParser } from "@langchain/core/output_parsers"
-// import { PromptTemplate } from "@langchain/core/prompts"
-// import { RunnableSequence } from "@langchain/core/runnables"
-// import * as dotenv from "dotenv"
-
-// dotenv.config()
-
-// const model = new ChatOpenAI()
-
-// const prompt1 = PromptTemplate.fromTemplate(
-//     `Which country has the highest population in Latin America? Respond only with the name of the country.`
-// )
-
-// const chain1 = prompt1
-//                 .pipe(model)
-//                 .pipe(new StringOutputParser())
-
-// const prompt2 = PromptTemplate.fromTemplate(
-//     `Tell me the currency of {country}?`
-// )
-
-// const chain2 = prompt2
-//                 .pipe(model)
-//                 .pipe(new StringOutputParser())
-
-// const completeChain = RunnableSequence.from([
-//     { country: chain1 },
-//     chain2
-// ])
-
-// const response = await completeChain.invoke()
-// console.log(response)
-
-// pass mutiple paramters to the nestad chains. 
-// ⛔️ Add picture ?
 import { ChatOpenAI } from "@langchain/openai"
 import { StringOutputParser } from "@langchain/core/output_parsers"
 import { PromptTemplate } from "@langchain/core/prompts"
@@ -80,6 +10,7 @@ const model = new ChatOpenAI()
 
 const prompt1 = PromptTemplate.fromTemplate(
     `Which country has the highest population in Latin America? Respond only with the name of the country.`
+    
 )
 
 const chain1 = prompt1
@@ -102,7 +33,5 @@ const completeChain = RunnableSequence.from([
     chain2
 ])
 
-
-// ⛔️ const response = await completeChain.invoke({word: 'capital'})
 const response = await completeChain.invoke({word: 'currency'})
 console.log(response)
