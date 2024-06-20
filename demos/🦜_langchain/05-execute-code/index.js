5b4e880c07f2f35902889c0fca178158f3288a83
Daniel <daniel.nastase@gmail.com>
2024-06-20 14:47:47 +0300

    05-execute-code code generation

---
+++ demos/🦜_langchain/05-execute-code/index.js
@@ -0,0 +1,31 @@
+
+import { ChatOpenAI } from "@langchain/openai"
+import { StringOutputParser } from "@langchain/core/output_parsers"
+import { PromptTemplate } from "@langchain/core/prompts"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const model = new ChatOpenAI({
+    modelName: "gpt-4o",
+    temperature: 0
+})
+
+// TODO Fix this 
+// const question = "What is (4 ^ 3 * 5 + 109 * 99 - 102 / 1000) / (2 ^ 5 * 3 - 10 + sqrt(81)). Just tell me the result."
+/*
+while you want it to be creative on some parst, you don't want to have a creative math equation AI
+there are cases hwere you want to run local , not very perofrmat modesl 
+*/
+
+
+const prompt = PromptTemplate.fromTemplate(
+    `You are an expert at writing correct executable JavaScript code. \n Generate JavaScript code to solve the the below question. Return the JavaScript code only, with no other explanation.  \n \n What is (4 ^ 3 * 5 + 109 * 99 - 102 / 1000) / (2 ^ 5 * 3 - 10 + sqrt(81))?`
+)
+
+const chain = prompt
+                .pipe(model)
+                .pipe(new StringOutputParser())
+
+const response = await chain.invoke()
+console.log(response)
