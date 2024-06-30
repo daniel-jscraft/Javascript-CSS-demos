2b7d6eaefdf2c6920fc503422b3e63ea00d8206c
Daniel <daniel.nastase@gmail.com>
2024-06-30 13:48:17 +0300

    08-routing-LLM-classifier setup

---
+++ demos/🦜_langchain/08-routing-LLM-classifier/index.js
@@ -0,0 +1,26 @@
+import { RunnableMap } from "@langchain/core/runnables"
+import { ChatOpenAI } from "@langchain/openai"
+import { StringOutputParser } from "@langchain/core/output_parsers"
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const model = new ChatOpenAI({})
+
+const frChain = ChatPromptTemplate.fromTemplate(
+    `Translate this to French {input}`
+).pipe(model).pipe(new StringOutputParser())
+
+const itPromt = ChatPromptTemplate.fromTemplate(
+    `Translate this to Italian {input}`
+).pipe(model).pipe(new StringOutputParser())
+
+
+const chain = RunnableMap.from({
+    "FR": frChain, 
+    "IT": itPromt
+})
+
+const result = await chain.invoke({input: 'Good morning, friend!'})
+console.log(result)
