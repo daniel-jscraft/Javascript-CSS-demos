46f166ef766f10cb69cd79278d517010861e73c8
Daniel <daniel.nastase@gmail.com>
2024-07-09 10:07:20 +0300

    12-sum3

---
+++ demos/🎙_interview/12-sum3.js
@@ -0,0 +1,39 @@
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+*/
+
+let x = [-1,0,1,2,-1,-4,-2,3,4]
+for (let i = 0; i < x.length; i++) 
+    for (let j = i+1; j < x.length; j++) 
+        for (let k = j+1; k < x.length; k++) 
+            if ( (i!=j) && (j!=k) && (k!=i))
+                if(x[i] + x[j] + x[k] == 0)  {
+                    console.log('----------------')
+                    console.log(`[${i} , ${j} , ${k}]`)
+                    console.log(`[${x[i]} , ${x[j]} , ${x[k]}]`) 
+                }   
