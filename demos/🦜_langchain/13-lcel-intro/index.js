6210a17bcaa5f86fb54027c7d80ab1a1d6859390
Daniel <daniel.nastase@gmail.com>
2024-08-16 11:59:31 +0300

    13-lcel-intro

---
+++ demos/🦜_langchain/13-lcel-intro/index.js
@@ -0,0 +1,20 @@
+import { ChatOpenAI } from "@langchain/openai"
+import { StringOutputParser } from "@langchain/core/output_parsers"
+import { ChatPromptTemplate } from "@langchain/core/prompts"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const model = new ChatOpenAI()
+const prompt = ChatPromptTemplate.fromTemplate(
+    `What contry is {celebrity} from? Tell me the {fact} of this contry?`
+)
+const parser = new StringOutputParser()
+
+const chain = prompt.pipe(model).pipe(parser)
+const response = await chain.invoke({
+    celebrity: `Shakira`, 
+    fact: `population`
+})
+console.log(response)
+// Shakira is from Colombia. The population of Colombia is approximately 50 million.
