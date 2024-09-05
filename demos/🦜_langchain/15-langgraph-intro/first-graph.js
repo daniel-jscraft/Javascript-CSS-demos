97e5289b242b8fb8d0c01b76ba3babe66adcc863
Daniel <daniel.nastase@gmail.com>
2024-09-05 15:11:47 +0300

    15-langgraph-intro ready

---
+++ demos/🦜_langchain/15-langgraph-intro/first-graph.js
@@ -0,0 +1,35 @@
+import { END, START, MessageGraph } from "@langchain/langgraph"
+import * as fs from "fs"
+
+const funcA = input => { input[0].content += " node A output"; return input }
+const funcB = input => { input[0].content += " node B output"; return input }
+
+// build the graph
+const graph = new MessageGraph()
+    // nodes
+    .addNode("nodeA", funcA)
+    .addNode("nodeB", funcB)
+    // edges
+    .addEdge(START, "nodeA")
+    .addEdge("nodeA", "nodeB")
+    .addEdge("nodeB", END)
+
+const runnable = graph.compile()
+const result = await runnable.invoke('Initial input ')
+console.log(result)
+
+// render the graph output
+const image = await runnable.getGraph().drawMermaidPng()
+const arrayBuffer = await image.arrayBuffer()
+await fs.writeFileSync('first-graph.png', new Uint8Array(arrayBuffer))
+
+/*
+[
+  HumanMessage {
+    "id": "675cb46c-2637-4ea5-ad35-1cde12615e49",
+    "content": "Initial input  node A output node B output",
+    "additional_kwargs": {},
+    "response_metadata": {}
+  }
+]
+*/
