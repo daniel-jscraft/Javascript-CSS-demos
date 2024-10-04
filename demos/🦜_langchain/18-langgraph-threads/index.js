31b0fb2bb355d9fb92cf24bfd4519b7afc20f24f
Daniel <daniel.nastase@gmail.com>
2024-10-04 14:56:28 +0300

    18-langgraph-threads code ready

--- demos/🦜_langchain/18-langgraph-threads/index.js
+++ demos/🦜_langchain/18-langgraph-threads/index.js
@@ -1,11 +1,8 @@
-import { HumanMessage, SystemMessage } from "@langchain/core/messages"
-import { ToolNode } from "@langchain/langgraph/prebuilt"
+import { HumanMessage } from "@langchain/core/messages"
 import {
-  END, START, StateGraph, MessagesAnnotation, Annotation, MemorySaver
+  END, START, StateGraph, MessagesAnnotation, MemorySaver
 } from "@langchain/langgraph"
 import { ChatOpenAI } from "@langchain/openai"
-import { tool } from "@langchain/core/tools"
-import { z } from "zod"
 import * as dotenv from "dotenv"
 
 dotenv.config()
@@ -13,48 +10,16 @@ dotenv.config()
 const llm = new ChatOpenAI({
   model: "gpt-4o",
   temperature: 0,
-});
-
-const getLastMessage = ({ messages }) => messages[messages.length - 1]
-
-const gmtTimeSchema = z.object({
-  city: z.string().describe("The name of the city")
 })
 
-const gmtTimeTool = tool(
-  async ({ city }) => {
-    const serviceIsWorking = Math.floor(Math.random() * 3)
-    return serviceIsWorking !== 2
-      ? `The local in ${city} time is 2:30am.`
-      : "Error 404"
-  },
-  {
-    name: "gmtTime",
-    description: `Check local time in a specified city. 
-    The API is randomly available every third call.`,
-    schema: gmtTimeSchema,
-  }
-)
-
-const tools = [gmtTimeTool]
-const toolNode = new ToolNode(tools)
-
+const getLastMessage = ({ messages }) => 
+  messages[messages.length - 1].content
 
 const callModel = async (state) => {
   const { messages } = state
-  const llmWithTools = llm.bindTools(tools)
-  const result = await llmWithTools.invoke(messages)
+  const result = await llm.invoke(messages)
   return { messages: [result] }
 }
-
-const shouldContinue = (state) => {
-  const lastMessage = getLastMessage(state)
-  const didAICalledAnyTools = lastMessage._getType() === "ai" &&
-    lastMessage.tool_calls?.length
-  return didAICalledAnyTools ? "tools" : END
-}
-
-
 
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
@@ -65,193 +30,50 @@ const workflow = new StateGraph(MessagesAnnotation)
 const checkpointer = new MemorySaver()
 const graph = workflow.compile({ checkpointer });
 
-const configA = {
-  configurable: { thread_id: "1" },
-  streamMode: "updates"
+
+console.log("👋 Starting intro thread")
+const configIntroThread = {
+  configurable: { thread_id: "t1" }
 }
+// First let's just introduce ourselves to the AI
+const t1r1 = await graph.invoke({
+  messages: [
+    new HumanMessage("hi! My name is Daniel and I like LangGraph!")
+],}, configIntroThread)
+console.log(getLastMessage(t1r1))
+//👉 Hi Daniel! How can I assist you with it today?
 
-// First let's just say hi to the AI
-for await (const update of await graph.stream({
-  messages: [{ role: "user", content: "hi! My name is Daniel and I like LangGraph!" }],
-}, configA)) {
-  console.log(update);
+// Does the AI remembers us?
+const t2r2 = await graph.invoke({
+  messages: [
+    new HumanMessage("Sorry, did I already introduce myself?")
+],}, configIntroThread)
+console.log(getLastMessage(t2r2))
+//👉 Yes, you did! You mentioned that your name is Daniel 
+// and that you like LangGraph.
+
+
+console.log("💬 Starting another thread")
+const configAnotherThread = {
+  configurable: { thread_id: "t2" }
 }
-
-// Let's continue the conversation (by passing the same config) and tell the AI we like pepperoni pizza
-for await (const update of await graph.stream({
-  messages: [{ role: "user", content: "Sorry, did I already introduce myself?" }],
-}, configA)) {
-  console.log(update);
-}
-
-const configB = {
-  configurable: { thread_id: "2" },
-  streamMode: "updates"
-}
-
-console.log("❌❌❌❌❌❌❌❌❌❌❌❌")
-for await (const update of await graph.stream({
-  messages: [{ role: "user", content: "Sorry, did I already introduce myself?" }],
-}, configB)) {
-  console.log(update);
-}
-
-// Yes, you did! You mentioned that your name is Daniel and that you like LangGraph. How can I assist you further?
-// No, you haven't introduced yourself yet. How can I assist you today?
-
-// import { HumanMessage, SystemMessage } from "@langchain/core/messages"
-// import { ToolNode } from "@langchain/langgraph/prebuilt"
-// import {
-//   END, START, StateGraph, MessagesAnnotation, Annotation
-// } from "@langchain/langgraph"
-// import { ChatOpenAI } from "@langchain/openai"
-// import { tool } from "@langchain/core/tools"
-// import { z } from "zod"
-// import * as dotenv from "dotenv"
-
-// dotenv.config()
-
-// const llm = new ChatOpenAI({
-//   model: "gpt-4o",
-//   temperature: 0,
-// });
-
-// const getLastMessage = ({ messages }) => messages[messages.length - 1]
-
-// const gmtTimeSchema = z.object({
-//   city: z.string().describe("The name of the city")
-// })
-
-// const gmtTimeTool = tool(
-//   async ({ city }) => {
-//     const serviceIsWorking = Math.floor(Math.random() * 3)
-//     return serviceIsWorking !== 2
-//       ? `The local in ${city} time is 2:30am.`
-//       : "Error 404"
-//   },
-//   {
-//     name: "gmtTime",
-//     description: `Check local time in a specified city. 
-//     The API is randomly available every third call.`,
-//     schema: gmtTimeSchema,
-//   }
-// )
-
-// const tools = [gmtTimeTool]
-// const toolNode = new ToolNode(tools)
-
-
-// const callModel = async (state) => {
-//   const { messages } = state
-//   const llmWithTools = llm.bindTools(tools)
-//   const result = await llmWithTools.invoke(messages)
-//   return { messages: [result] }
-// }
-
-// const shouldContinue = (state) => {
-//   const lastMessage = getLastMessage(state)
-//   const didAICalledAnyTools = lastMessage._getType() === "ai" &&
-//     lastMessage.tool_calls?.length
-//   return didAICalledAnyTools ? "tools" : END
-// }
-
-// const workflow = new StateGraph(MessagesAnnotation)
-//   .addNode("agent", callModel)
-//   .addNode("tools", toolNode)
-//   .addEdge(START, "agent")
-//   .addEdge("tools", "agent")
-//   .addConditionalEdges("agent", shouldContinue, ["tools", END])
-
-// const graph = workflow.compile();
-
-// const configA = {
-//   "configurable": { thread_id: "soccer" },
-// }
-
-// let input = {
-//   messages: [
-//     {
-//       role: "user",
-//       content: "Hello, I am John",
-//     },
-//   ],
-// };
-
-
-
-// let result = await graph.invoke(input, configA)
-// console.log(result)
-
-// input = {
-//   messages: [
-//     {
-//       role: "user",
-//       content: "Sorry, did I already introduce myself?",
-//     },
-//   ],
-// }
-
-// result = await graph.invoke(input, configA)
-// console.log(result)
-
-
+// This is a brand new thread with no prior knowledge
+const t2r1 = await graph.invoke({
+  messages: [
+    new HumanMessage("Sorry, did I already introduce myself?")
+],}, configAnotherThread)
+console.log(getLastMessage(t2r1))
+//👉 No, you haven't introduced yourself yet. How can I assist you today?
 
 /*
 
 checkout this : https://langchain-ai.github.io/langgraphjs/how-tos/shared-state/?h=thread+id
 
-is it safe to say that we are not even using a real AI here
+the structure of the graph is extremly simple
+is it safe to say that we are not even using a real AI Agent here
 but bear with me. this will be usefull for the next chapers
 
 ia text din  https://www.youtube.com/watch?v=9BPCV5TYPmg
 
-poza open ai discution threads
-
-  messages: [
-update  la ai message
+poza printrscreen chat gpt discution threads
 */
-
-// async function main() {
-//   const config = {
-//     configurable: { thread_id: "refunder" },
-//     streamMode: "updates",
-//   };
-//   const input = {
-//     messages: [
-//       {
-//         role: "user",
-//         content: "Can I have a refund for my purchase? Order no. 123",
-//       },
-//     ],
-//   };
-
-//   for await (const event of await graph.stream(input, config)) {
-//     const key = Object.keys(event)[0];
-//     if (key) {
-//       console.log(`Event: ${key}\n`);
-//     }
-//   }
-
-//   console.log("\n---INTERRUPTING GRAPH TO UPDATE STATE---\n\n");
-
-//   console.log(
-//     "---refundAuthorized value before state update---",
-//     (await graph.getState(config)).values.refundAuthorized
-//   );
-
-//   await graph.updateState(config, { refundAuthorized: true });
-
-//   console.log(
-//     "---refundAuthorized value after state update---",
-//     (await graph.getState(config)).values.refundAuthorized
-//   );
-
-//   console.log("\n---CONTINUING GRAPH AFTER STATE UPDATE---\n\n");
-
-//   for await (const event of await graph.stream(null, config)) {
-//     // Log the event to the terminal
-//     logEvent(event);
-//   }
-// }
-
-// main();
