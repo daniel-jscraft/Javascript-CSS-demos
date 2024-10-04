6fac5089578dcde227252555ccf591b0785a83e0
Daniel <daniel.nastase@gmail.com>
2024-10-04 14:14:00 +0300

    18-langgraph-threads working example

--- demos/🦜_langchain/18-langgraph-threads/index.js
+++ demos/🦜_langchain/18-langgraph-threads/index.js
@@ -1,7 +1,7 @@
 import { HumanMessage, SystemMessage } from "@langchain/core/messages"
 import { ToolNode } from "@langchain/langgraph/prebuilt"
 import {
-  END, START, StateGraph, MessagesAnnotation, Annotation
+  END, START, StateGraph, MessagesAnnotation, Annotation, MemorySaver
 } from "@langchain/langgraph"
 import { ChatOpenAI } from "@langchain/openai"
 import { tool } from "@langchain/core/tools"
@@ -54,44 +54,132 @@ const shouldContinue = (state) => {
   return didAICalledAnyTools ? "tools" : END
 }
 
+
+
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
   .addNode("tools", toolNode)
   .addEdge(START, "agent")
   .addEdge("tools", "agent")
   .addConditionalEdges("agent", shouldContinue, ["tools", END])
-
-const graph = workflow.compile();
+  
+// Initialize memory to persist state between graph runs
+const checkpointer = new MemorySaver()
+const graph = workflow.compile({ checkpointer });
 
 const configA = {
-  "configurable": { thread_id: "soccer" },
+  configurable: { thread_id: "1" },
+  streamMode: "updates"
 }
 
-let input = {
-  messages: [
-    {
-      role: "user",
-      content: "Hello, I am John",
-    },
-  ],
-};
-
-
-
-let result = await graph.invoke(input, configA)
-console.log(result)
-
-input = {
-  messages: [
-    {
-      role: "user",
-      content: "Sorry, did I already introduce myself?",
-    },
-  ],
+// First let's just say hi to the AI
+for await (const update of await graph.stream({
+  messages: [{ role: "user", content: "hi! My name is Daniel and I like LangGraph!" }],
+}, configA)) {
+  console.log(update);
 }
 
-result = await graph.invoke(input, configA)
-console.log(result)
+// Let's continue the conversation (by passing the same config) and tell the AI we like pepperoni pizza
+for await (const update of await graph.stream({
+  messages: [{ role: "user", content: "Sorry, did I already introduce myself?" }],
+}, configA)) {
+  console.log(update);
+}
+
+// import { HumanMessage, SystemMessage } from "@langchain/core/messages"
+// import { ToolNode } from "@langchain/langgraph/prebuilt"
+// import {
+//   END, START, StateGraph, MessagesAnnotation, Annotation
+// } from "@langchain/langgraph"
+// import { ChatOpenAI } from "@langchain/openai"
+// import { tool } from "@langchain/core/tools"
+// import { z } from "zod"
+// import * as dotenv from "dotenv"
+
+// dotenv.config()
+
+// const llm = new ChatOpenAI({
+//   model: "gpt-4o",
+//   temperature: 0,
+// });
+
+// const getLastMessage = ({ messages }) => messages[messages.length - 1]
+
+// const gmtTimeSchema = z.object({
+//   city: z.string().describe("The name of the city")
+// })
+
+// const gmtTimeTool = tool(
+//   async ({ city }) => {
+//     const serviceIsWorking = Math.floor(Math.random() * 3)
+//     return serviceIsWorking !== 2
+//       ? `The local in ${city} time is 2:30am.`
+//       : "Error 404"
+//   },
+//   {
+//     name: "gmtTime",
+//     description: `Check local time in a specified city. 
+//     The API is randomly available every third call.`,
+//     schema: gmtTimeSchema,
+//   }
+// )
+
+// const tools = [gmtTimeTool]
+// const toolNode = new ToolNode(tools)
+
+
+// const callModel = async (state) => {
+//   const { messages } = state
+//   const llmWithTools = llm.bindTools(tools)
+//   const result = await llmWithTools.invoke(messages)
+//   return { messages: [result] }
+// }
+
+// const shouldContinue = (state) => {
+//   const lastMessage = getLastMessage(state)
+//   const didAICalledAnyTools = lastMessage._getType() === "ai" &&
+//     lastMessage.tool_calls?.length
+//   return didAICalledAnyTools ? "tools" : END
+// }
+
+// const workflow = new StateGraph(MessagesAnnotation)
+//   .addNode("agent", callModel)
+//   .addNode("tools", toolNode)
+//   .addEdge(START, "agent")
+//   .addEdge("tools", "agent")
+//   .addConditionalEdges("agent", shouldContinue, ["tools", END])
+
+// const graph = workflow.compile();
+
+// const configA = {
+//   "configurable": { thread_id: "soccer" },
+// }
+
+// let input = {
+//   messages: [
+//     {
+//       role: "user",
+//       content: "Hello, I am John",
+//     },
+//   ],
+// };
+
+
+
+// let result = await graph.invoke(input, configA)
+// console.log(result)
+
+// input = {
+//   messages: [
+//     {
+//       role: "user",
+//       content: "Sorry, did I already introduce myself?",
+//     },
+//   ],
+// }
+
+// result = await graph.invoke(input, configA)
+// console.log(result)
 
 
 
