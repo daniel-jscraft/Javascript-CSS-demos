57b20ce5a86871a5c8c967fd85849fa357079f92
Daniel <daniel.nastase@gmail.com>
2024-12-02 14:30:49 +0100

    22-langraph-multi-agent research agent initial prompt

--- demos/🦜_langchain/22-langraph-multi-agent/agents/researcher.js
+++ demos/🦜_langchain/22-langraph-multi-agent/agents/researcher.js
@@ -1,14 +1,28 @@
 import { getLastMessage } from "../etc/utils.js"
-import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
-import { ChatOpenAI } from "@langchain/openai";
-import { END, START, StateGraph, MessagesAnnotation } from "@langchain/langgraph";
+import { TavilySearchResults } from "@langchain/community/tools/tavily_search"
+import { SystemMessage } from "@langchain/core/messages"
+import { ChatOpenAI } from "@langchain/openai"
+import { END, START, Annotation, messagesStateReducer, StateGraph } from "@langchain/langgraph"
 import { ToolNode } from "@langchain/langgraph/prebuilt"
 import * as dotenv from "dotenv"
+
 dotenv.config()
 
 const llm = new ChatOpenAI({
     modelName: "gpt-4o",
     temperature: 0
+})
+
+export const researcherAnnotation = Annotation.Root({
+  messages: Annotation({
+    reducer: messagesStateReducer,
+    default: () => [
+        new SystemMessage("You are a web researcher. You may use the" + 
+            " Tavily search engine to search the web for important" +
+            " information, so the Chart Generator in your team can" +
+            " make useful plots.")
+    ]
+  })
 })
 
 const tavilyTool = new TavilySearchResults()
@@ -28,7 +42,7 @@ const shouldContinue = (state) => {
     return didAICalledAnyTools ? "tools" : END
 }
 
-const researcherGraph = new StateGraph(MessagesAnnotation)
+const researcherGraph = new StateGraph(researcherAnnotation)
     .addNode("agent", callModel)
     .addNode("tools", toolNode)
     .addEdge(START, "agent")
