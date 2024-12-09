ad0dff4e2e7180664bcd12c0a7bcb1d3404d2928
Daniel <daniel.nastase@gmail.com>
2024-12-09 10:47:58 +0100

    18-binary-tree.js

---
+++ demos/🎙_interview/18-binary-tree.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinaryTree {
+    constructor() {
+        this.root = null
+    }
+
+    insert(val) {
+        const newNode = new Node(val)
+        this.root === null ? 
+            this.root = newNode :
+            this._insert(newNode, this.root)
+    }
+
+    _insert(newNode, cNode) {
+        if (cNode === null) 
+            return cNode = newNode
+        if (cNode.val > newNode.val) {
+            cNode.left === null ?
+                cNode.left = newNode :
+                this._insert(newNode, cNode.left)
+        }
+        else {
+            cNode.right === null ?
+                cNode.right = newNode :
+                this._insert(newNode, cNode.right)
+        }
+    }
+
+    printRooLeftRight(cNode) {
+        if (cNode === null) return
+        console.log(cNode.val)
+        this.printRooLeftRight(cNode.left)
+        this.printRooLeftRight(cNode.right)
+    }
+}
+
+
+
+const myTree = new BinaryTree() 
+myTree.insert(10)
+myTree.insert(5)
+myTree.insert(15)
+myTree.insert(3)
+myTree.insert(7)
+myTree.insert(12)
+myTree.insert(18)
+
+myTree.printRooLeftRight(myTree.root)
+
+//             10
+//     5               15
+// 3       7       12      18
+
+// Pre-order Traversal ; (Root → Left → Right):
+// 10, 5, 3, 7, 15, 12, 18
+    
