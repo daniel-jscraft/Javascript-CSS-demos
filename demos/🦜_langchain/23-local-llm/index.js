e155b6b7ed8bbb53ed1876da3b09fdfe89d8aee5
Daniel <daniel.nastase@gmail.com>
2025-02-05 09:27:24 +0100

    local llms

---
+++ demos/🦜_langchain/23-local-llm/index.js
@@ -0,0 +1,9 @@
+import { Ollama } from '@langchain/ollama';
+
+const llm = new Ollama({
+  baseUrl: "http://localhost:11435",
+  model: "llama3.2"
+})
+
+let response = await llm.invoke('What is the age of Bugs Bunny?')
+console.log(response)
