558947bb188a66018c1b991e4137e2a9d8c971c5
Daniel <daniel.nastase@gmail.com>
2025-03-04 10:48:47 +0200

    Remove __dirname and __filename imports from multimodal agent tools

--- demos/🦜_langchain/25-multimodal-agent/tools/index.js
+++ demos/🦜_langchain/25-multimodal-agent/tools/index.js
@@ -1,8 +1,2 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 export { default as readAudioFileTool } from './audio-tool.js';
 export { default as readImageFileTool } from './image-tool.js'; 
